7467144632b0a7291025c8519f1622450b0210d6
Alex Young <45477688+alex-l-young@users.noreply.github.com>
2026-02-24 17:12:26 -0600

    Initial Commit

---
+++ Trillium/TrilliumApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrilliumApp.swift
+//  Trillium
+//
+//  Created by Alex Young on 2/24/26.
+//
+
+import SwiftUI
+
+@main
+struct TrilliumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
